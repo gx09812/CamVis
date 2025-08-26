853b4c2593d47b4f549ec70cb9a1e3ddd3c8848b
gx09812 <technik0369@gmail.com>
2025-08-27 00:19:14 +0530

    used cv2 to check

--- ai_alg.py
+++ ai_alg.py
@@ -1,1 +1,17 @@
-print("hello world")
+import cv2 
+
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = video_capture.read()
+    if not ret:
+        print("Failed to capture video")
+        break
+
+    cv2.imshow("Drone Camera", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
